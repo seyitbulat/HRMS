856dd4298a118a9e8bc8a3ddcc2633ee663db179
Seyit Bulat <mseyit68@gmail.com>
2023-12-19 09:20:27 +0300

    tested department endpoints

--- HRMS.Business/AutoMapperProfiles.vb
+++ HRMS.Business/AutoMapperProfiles.vb
@@ -8,5 +8,9 @@ Public Class AutoMapperProfiles : Inherits Profile
         CreateMap(Of LeavesType, LeavesTypeGetDto)()
         CreateMap(Of LeavesTypePostDto, LeavesType)()
         CreateMap(Of LeavesTypePutDto, LeavesType)()
+
+        CreateMap(Of Department, DepartmentGetDto)()
+        CreateMap(Of DepartmentPostDto, Department)()
+        CreateMap(Of DepartmentPutDto, Department)()
     End Sub
 End Class
