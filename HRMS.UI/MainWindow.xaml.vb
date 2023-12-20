49f0b880454ae1e45d31500a011743574f4b906d
Tuğçe Dokgöz <tugcedokgoz201@gmail.com>
2023-12-20 09:19:22 +0300

    department get
    department get

--- HRMS.UI/MainWindow.xaml.vb
+++ HRMS.UI/MainWindow.xaml.vb
@@ -6,11 +6,19 @@ Imports DevExpress.Xpf.Core
 ''' </summary>
 Partial Public Class MainWindow
     Inherits ThemedWindow
+
     Public Sub New()
         InitializeComponent()
-        'MainContentFrame.Navigate(New HomePage())
-        'MainContentFrame1.Navigate(New CandidateTable())
-        MainContentFrame2.Navigate(New EmployeeTable())
+        ''MainContentFrame.Navigate(New HomePage())
+        ''MainContentFrame1.Navigate(New CandidateTable())
+        ''MainContentFrame2.Navigate(New EmployeeTable())
+        'MainContentFrame3.Navigate(New DepartmentTable())
 
+    End Sub
+
+
+    Private Sub Department_Click(sender As Object, e As RoutedEventArgs)
+        Dim departmentTable As New DepartmentTable()
+        pageView.Content = departmentTable
     End Sub
 End Class
--- HRMS.UI/Table/DepartmentTable.xaml.vb
+++ HRMS.UI/Table/DepartmentTable.xaml.vb
@@ -1,3 +1,32 @@
-﻿Class DepartmentTable
+﻿Imports DevExpress.Xpf.Grid
+Imports Newtonsoft.Json
+Imports System.Collections.ObjectModel
+Imports System.Net.Http
+Imports System.Threading.Tasks
 
+Partial Public Class DepartmentTable
+    Inherits Page
+
+    Public Sub New()
+        InitializeComponent()
+        AddHandler Me.Loaded, AddressOf Page_Loaded
+    End Sub
+
+    Private Async Sub Page_Loaded(sender As Object, e As RoutedEventArgs)
+        Dim departments As ObservableCollection(Of Department) = Await GetDepartmentsAsync()
+        DepartmentGrid.ItemsSource = departments
+    End Sub
+
+    Public Async Function GetDepartmentsAsync() As Task(Of ObservableCollection(Of Department))
+        Using client As New HttpClient()
+            Dim response As HttpResponseMessage = Await client.GetAsync("https://localhost:50099/Department/GetAll")
+            If response.IsSuccessStatusCode Then
+                Dim json As String = Await response.Content.ReadAsStringAsync()
+                Return JsonConvert.DeserializeObject(Of ObservableCollection(Of Department))(json)
+            Else
+                ' Handle error situation
+                Return New ObservableCollection(Of Department)()
+            End If
+        End Using
+    End Function
 End Class
