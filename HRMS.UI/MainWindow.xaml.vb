fb444b58281ab816b0bd62755aac0f4966365b57
Tuğçe Dokgöz <tugcedokgoz201@gmail.com>
2023-12-22 15:39:53 +0300

    neler neler yaptım neler
    neler neler

---
+++ HRMS.UI/MainWindow.xaml.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Text
+Imports DevExpress.Xpf.Core
+
+Namespace HRMS.UI
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits ThemedWindow
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub DataGrid_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
+
+        End Sub
+    End Class
+End Namespace
---
+++ HRMS.UI/Page/DepartmentPage.xaml.vb
@@ -0,0 +1,3 @@
+﻿Public Class DepartmentPage
+
+End Class
